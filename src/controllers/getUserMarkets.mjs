180e0b3acdcac3c24be7ac71cc299773754034ad
antonkravchenko2001 <antonio57banderas@gmail.com>
2023-07-30 14:18:04 +0000

    added  /users/{userAddress}/markets controller

---
+++ src/controllers/getUserMarkets.mjs
@@ -0,0 +1,93 @@
+export const getUserMarkets = async(db, params) => {
+    const usersCollection = await db.collection("users");
+
+    const offset = params['offset'] || 0
+    const limit = params['limit'] || 10
+    const sortOrder = (
+        (
+            (params['order'] == null) ||
+            (params['order'] === 'desc')
+        ) ?
+        -1 :
+        1
+    );
+
+    const matchParams = [];
+    matchParams.push(
+        {
+            "address": params.userAddress
+        }
+    );
+    if(params['resolved'] == true){
+        matchParams.push(
+            {
+                "markets.status": "resolved"
+            }
+        )
+    }
+    if(params['receivedReward'] != null){
+        matchParams.push(
+            {
+                "reputation": {$ne : null}
+            }
+        )
+    }
+
+
+
+
+    return await usersCollection.aggregate([
+        {
+            $match: {
+                $and: matchParams
+            }
+        },
+        {
+            $sort: {
+                "markets.predictionDatetime": sortOrder
+            }
+        },
+        {
+           $project: {
+                _id: 0,
+                address: "$markets.address",
+                question: "$markets.question",
+                creationDatetime: "$markets.creationDatetime",
+                wageDeadline: "$markets.wageDeadline",
+                resolutionDate: "$markets.resolutionDate",
+                topic: "$markets.topic",
+                reputationTokenAddress: "$markets.reputationTokenAddress",
+                options: "$markets.options",
+                status: "$markets.status",
+                answer: {
+                    $cond: {
+                        if: { $ifNull: ['$markets.answer', false] },
+                        then: "$markets.answer",
+                        else: null
+                    },
+                },
+                reputation: {
+                    $cond: {
+                        if: { $ifNull: ['$markets.reputation', false] },
+                        then: "$markets.reputation",
+                        else: null
+                    },
+                },
+                correct: {
+                    $cond: {
+                        if: {$eq:["$markets.decodedPrediction", "$markets.answer"]},
+                        then: true,
+                        else: false
+                    },
+                }
+            }
+        },
+        {
+            $skip: offset
+        },
+        {
+            $limit: limit
+        }
+     ])
+     .toArray();
+}
---
+++ tests/controllers/test_getUserMarkets.mjs
@@ -0,0 +1,94 @@
+import chai, { expect } from "chai";
+import { dbTest as db} from "../../src/models/mongo_setup.mjs";
+import { 
+    getUserMarkets
+} from "../../src/controllers/getUserMarkets.mjs";
+
+describe('testing /users/{userAddress}/markets  controller', async function() {
+
+    beforeEach(async function () {
+        const users = await db.collection("users");
+
+        await users.deleteMany(
+            {}
+        );
+        
+    });
+
+    afterEach(async function () {
+        const users = await db.collection("users");
+        await users.deleteMany(
+            {}
+        );
+    });
+
+  
+    // test a functionality
+    it('testing return fields', async function() {
+        let users = db.collection("users");
+        const userParams = {
+            "address": '0x',
+            "description": "hello",
+            "status": "subscribed",
+            "registrationDate": 1e6,
+            "markets": [
+                {
+                    "address": "0x1",
+                    "question": "question",
+                    "creationDatetime": 1e6,
+                    "wageDeadline": 1e6,
+                    "resolutionDate": 1e6,
+                    "topic": "topic",
+                    "reputationTokenAddress": "0x3",
+                    "status": "resolved",
+                    "answer": 9,
+                    "decodedPrediction": 8,
+                    "reputaion": 9.1,
+                    "options": ["1", "2"]
+                },
+                {
+                    "address": "0x2",
+                    "question": "question",
+                    "creationDatetime": 1e6,
+                    "wageDeadline": 1e6,
+                    "status": "unresolved",
+                    "resolutionDate": 1e6,
+                    "topic": "topic",
+                    "reputationTokenAddress": "0x3",
+                    "answer": null,
+                    "decodedPrediction": null,
+                    "reputaion": null,
+                    "options": ["1", "2"]
+                }
+            ]
+
+        }
+
+        await users.insertOne(userParams);
+        const params = {
+            "userAddress": "0x",
+            "resolved": true
+        }
+        const results = (
+            await getUserMarkets(
+                db,
+                params
+            )
+        )[0];
+        const fieldsToReturn = [
+            'address',
+            'question',
+            'creationDatetime',
+            'wageDeadline',
+            'resolutionDate',
+            'topic',
+            'reputationTokenAddress',
+            'options',
+            'status',
+            'answer',
+            'reputation',
+            'correct'
+        ]
+        expect(fieldsToReturn).to.eql(Object.keys(results));
+    })
+})
